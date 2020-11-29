2eaa67b7f9560660c13292f28abbc4667b56decd
Cece Claridge <cece.claridge@gmail.com>
2020-11-29 18:24:03 -0500

    basic functionality for querying Yelp API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { AppContextProvider } from './context/AppContext';
 import ContextDemo from './components/ContextDemo';
+import axios from 'axios';
 
 import './App.css';
 
 const App = () => {
-  const [serverMessage, setServerMessage] = useState('');
-
-  const fetchDemoData = () => {
-    fetch('/api/demo')
-      .then((response) => response.json())
-      .then((data) => setServerMessage(data.message));
+  const [businesses, setBusinesses] = useState([]);
+  const fetchYelpData = async () => {
+    try {
+      const resp = await axios.get('/api/businesses');
+      setBusinesses(resp.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  useEffect(fetchDemoData, []);
+  useEffect(fetchYelpData, []);
 
   return (
     <AppContextProvider>
-      <div id="demo">
-        <h3>Hello from client/src/App.js</h3>
-        <ContextDemo />
-        <h3>{serverMessage}</h3>
+      <div>
+        {businesses.length &&
+          businesses.map((business) => {
+            return (
+              <div key={business.id}>
+                <h2>{business.name}</h2>
+                <img src={business.image_url} alt="business-pic" />
+              </div>
+            );
+          })}
       </div>
     </AppContextProvider>
   );
--- server.js
+++ server.js
@@ -5,12 +5,28 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const path = require('path');
 const app = express();
+const axios = require('axios');
 
 // JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
-app.get('/api/demo', (request, response) => {
-  response.json({
-    message: 'Hello from server.js'
-  });
+
+const getYelpAPI = async () => {
+  return axios.get(
+    `https://api.yelp.com/v3/businesses/search?location="2650 NW 5 Ave, Miami, FL 33127"&term="dinner"&radius=40000`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.YELP_API_KEY}`
+      }
+    }
+  );
+};
+
+app.get('/api/businesses', async (request, response) => {
+  try {
+    const resp = await getYelpAPI();
+    response.json(resp.data.businesses);
+  } catch (error) {
+    console.log(error);
+  }
 });
 // END DEMO
 
